7ba26a9f26f3c139d701a4a4f66a3bea8c53efd8
catherine_guo <catherine_guo@realsil.com.cn>
2024-04-24 16:56:42 +0800

    check the env
    Change-Id: I60e6bd93e66ac36ffbf26141c732b5db6bdae393

--- script/cmd.py
+++ script/cmd.py
@@ -2,8 +2,10 @@ import os
 print("This is on pr")
 
 SECRET_PWDs = ""
-SECRET_PWDs = os.environ.get("SECRET_PWDs")
+SECRET_PWDs = os.environ.get("SECRET_PWD")
 if SECRET_PWDs: 
-    print("env", os.environ.get("SECRET_PWDs"))
+    print("env", os.environ.get("SECRET_PWD"))
 else:
     print("env is None")
+
+print("please check the env")
