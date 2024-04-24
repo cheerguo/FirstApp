6431b03f7a686ccf144211b2dc071a8494869385
catherine_guo <catherine_guo@realsil.com.cn>
2024-04-24 16:39:06 +0800

    print secret
    Change-Id: If6d2b0bf314e07b0feae0c99ff9eb748b80c44e9

--- script/cmd.py
+++ script/cmd.py
@@ -1,1 +1,9 @@
+import os
 print("This is on pr")
+
+SECRET_PWDs = ""
+SECRET_PWDs = os.environ.get("SECRET_PWDs")
+if SECRET_PWDs: 
+    print("env", os.environ.get("SECRET_PWDs"))
+else:
+    print("env is None")
